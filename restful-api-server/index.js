adfb07f289ba5b3ed47125f443cb7ca0719dea2c
bachdx <bachdx.hut@gmail.com>
2019-01-01 02:40:23 +0900

    add router

--- restful-api-server/index.js
+++ restful-api-server/index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should response to all requests with a string
 const server = http.createServer(function(req, res) {
@@ -17,14 +18,88 @@ const server = http.createServer(function(req, res) {
   const path = parseUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-  // Send the response
-  res.end("heeloo world\n");
+  // Get the query string as an object
+  const queryStringObject = parseUrl.query;
 
-  // Log the request path
-  console.log(`request received on path: ${trimmedPath}`)
+  // Get the HTTP Method
+  const method = req.method.toLowerCase();
+
+  // Get the headers as an object
+  const headers = req.headers;
+
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  var buffer = '';
+
+  req.on('data', function(data){
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', function() {
+    buffer += decoder.end();
+
+    // Choose the handlers
+    // If not found then return notFound
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handler.notFound;
+
+    // Construct the data object to send to the handler
+    const data = {
+      trimmedPath: trimmedPath,
+      queryStringObject: queryStringObject,
+      method: method,
+      headers: headers,
+      payload: buffer
+    };
+
+    // route the request to the handler specified in the router
+    chosenHandler(data, function(statusCode, payload) {
+      // Use the status code called back by the handler, or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      // User the payload called by the handler, or default to an empty object
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // Convert the payload to a String
+      const payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.writeHead(statusCode);
+
+      // Send the response
+      res.end(payloadString);
+      // Log the request path
+      console.log(`request received on path: ${trimmedPath} with method : ${method}`)
+      // Log the query string object
+      console.log("query string params:", queryStringObject)
+      // Log the headers
+      console.log("headers:", headers);
+      // Log the headers
+      console.log("Payload:", buffer);
+
+      console.log("returning the response: ", statusCode, payloadString);
+    });
+  });
 });
 
 // Start the server, and have it listen on port 3000
 server.listen(3000, function() {
   console.log("the server is listening on port 3000");
 });
+
+// Define the handlers
+const handler = {};
+
+// Sample handler
+handler.sample = function(data, callback) {
+  // Callback a http status cod, and a payload object
+  callback(406, { name: 'sample handler' });
+};
+
+// Not found handler
+handler.notFound = function(data, callback){
+  callback(404);
+};
+
+// Define a request router
+const router = {
+  sample: handler.sample
+}
